96087c20a5f9de8acf575a6b1c9b9ac8149d209e
danielordonezg <danielale.gomez@correo.usa.edu.co>
2025-03-31 13:12:47 -0500

    deploy

--- src/services/general.services.js
+++ src/services/general.services.js
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 
 import { SECRET } from "../config.js";
+import Role from "../models/Role.js";
 
 export const regexPassword =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[~!@#$%^&*()_+{}|:;'<>,./?])[a-zA-Z0-9~!@#$%^&*()_+{}|:;'<>,./?]{10,}$/;
@@ -19,13 +20,9 @@ export const signUp = async (body) => {
       password: bcrypt.hashSync(password, 8),
     });
 
-    if (roles) {
-      const foundRoles = await Role.find({ name: { $in: roles } });
-      newUser.roles = foundRoles.map((role) => role._id);
-    } else {
-      const role = await Role.findOne({ name: "user" });
-      newUser.roles = [role._id];
-    }
+    const foundRoles = await Role
+    .find({ name: { $in: roles } });
+    newUser.roles = foundRoles.map((role) => role._id);
 
     const savedUser = await newUser.save();
 
